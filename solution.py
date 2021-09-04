09e42a74d9a93a91846e5e0f4f46b0bfd1d59ce3
osalvatierra <os28@nyu.edu>
2021-09-04 15:05:00 -0400

    updating solution.py v3

--- solution.py
+++ solution.py
@@ -6,7 +6,9 @@
 def welcome_assignment_answers(question):
     #The student doesn't have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-    if question == "Is it possible to decrypt a message without a key? - Yes/No":
+    if question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
         answer = "mTLS"
@@ -23,9 +25,9 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = "4"
+        answer = 4
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = "3"
+        answer = 3
     return(answer)
 # Complete all the questions.
 
