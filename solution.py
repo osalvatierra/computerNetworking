6556fadee7445ad13767de7e7a6cf36baddd60cd
osalvatierra <oscar@owebdesigns.com>
2021-09-28 21:49:50 -0400

    smtp v6

--- solution.py
+++ solution.py
@@ -82,6 +82,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
         print('250 reply not received from server.')
     # Fill in end
 
+    clientsocket.close()
+
 
 if __name__ == '__main__':
     smtp_client(1025, '127.0.0.1')
