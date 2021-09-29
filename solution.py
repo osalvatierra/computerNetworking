2daa63629ae1f14209f18565f016b7413a9c6b22
osalvatierra <oscar@owebdesigns.com>
2021-09-28 22:45:57 -0400

    smtp v21

--- solution.py
+++ solution.py
@@ -60,6 +60,10 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Send message data.
     # Fill in start
     clientSocket.send(msg.encode())
+    recv1 = clientSocket.recv(1024).decode()
+    # print(recv1)
+    if recv1[:3] != '250':
+        print('250 reply not received from server.')
     # Fill in end
 
     # Message ends with a single period.
